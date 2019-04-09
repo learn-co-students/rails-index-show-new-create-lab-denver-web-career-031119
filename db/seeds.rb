6e2f6e252a48e781a169ce7030d134737880947c
github username <github email address>
2019-04-09 10:55:53 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: "ALK234", store: "Sprouts")
+Coupon.create(coupon_code: "POE467", store: "Taco Bell")
